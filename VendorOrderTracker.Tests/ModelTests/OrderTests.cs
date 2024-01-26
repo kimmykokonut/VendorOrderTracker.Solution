e2277ad4fbf87d0a5c39e54b51b55a40a4a4ecab
Kim Robinson <kimmykokonut@gmail.com>
2024-01-26 10:27:59 -0800

    pass test for get description property

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -9,14 +9,14 @@ namespace VendorOrderTracker.TestTools
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Scone");
+      Order newOrder = new Order("Scone", "yummy");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     [TestMethod]
     public void GetTitle_ReturnsTitle_String()
     {
       string title = "Scone";
-      Order testOrder = new Order(title);
+      Order testOrder = new Order(title, "yummy");
       string result = testOrder.Title;
       Assert.AreEqual(title, result);
     }
@@ -24,11 +24,20 @@ namespace VendorOrderTracker.TestTools
     public void SetTitle_SetsValueOfTitle_String()
     {
       string title = "Scone";
-      Order testOrder = new Order(title);
+      Order testOrder = new Order(title, "yummy");
       string updatedTitle = "Danish";
       testOrder.Title = updatedTitle;
       string result = testOrder.Title;
       Assert.AreEqual(updatedTitle, result);
+    }
+    [TestMethod]
+    public void GetDescription_ReturnsValueOfDescription_String()
+    {
+      string title = "Scone";
+      string description = "A biscuit-like treat full of butter";
+      Order testOrder = new Order(title, description);
+      string result = testOrder.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -3,10 +3,12 @@ namespace VendorOrderTracker.Models
   public class Order
   {
     public string Title { get; set; }
+    public string Description { get; }
 
-    public Order(string title)
+    public Order(string title, string description)
     {
       Title = title;
+      Description = description;
     }
   }
 }
