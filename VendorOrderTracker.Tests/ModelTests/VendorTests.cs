92df03e886bccf9d78188701c03a8edea9322672
Kim Robinson <kimmykokonut@gmail.com>
2024-01-26 09:50:43 -0800

    pass test for get value of description property

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -9,14 +9,14 @@ namespace VendorOrderTracker.TestTools
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Sweetpeas");
+      Vendor newVendor = new Vendor("Sweetpeas", "Vegan bakery");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]
     public void GetName_ReturnsName_String()
     {
       string name = "Little T";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "a place");
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
     }
@@ -24,11 +24,20 @@ namespace VendorOrderTracker.TestTools
     public void SetName_SetsValueOfName_String()
     {
       string name = "Little T";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "a place");
       string updatedName = "Petite Provence";
       newVendor.Name = updatedName;
       string result = newVendor.Name;
       Assert.AreEqual(updatedName, result);
+    }
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string name = "Little T";
+      string description = "A neighborhood bakery specializing in locally sourced ingredients";
+      Vendor newVendor = new Vendor(name, description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
 
 
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -3,9 +3,11 @@ namespace VendorOrderTracker.Models
   public class Vendor
   {
     public string Name { get; set; }
-    public Vendor(string name)
+    public string Description { get; }
+    public Vendor(string name, string description)
     {
       Name = name;
+      Description = description;
     }
   }
 }
