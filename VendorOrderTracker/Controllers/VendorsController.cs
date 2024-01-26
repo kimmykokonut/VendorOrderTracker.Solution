668219be351c31de014ee21bc6f2478a3ceea0eb
Kim Robinson <kimmykokonut@gmail.com>
2024-01-26 14:08:55 -0800

    add vendor detail page to controller and show view

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -17,5 +17,20 @@ namespace VendorOrderTracker.Controllers
     {
       return View();
     }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vName, string vDescription, string vPhone)
+    {
+      Vendor newVendor = new Vendor(vName, vDescription, vPhone);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/{vId}")]
+    public ActionResult Show(int vId)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(vId);
+      //add orders list later once orders created
+      model.Add("vendor", selectedVendor);
+      return View(model);
+    }
   }
 }
